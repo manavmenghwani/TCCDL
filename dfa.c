bb895f1993e0a52f91fa382cc306fe67c5ec0b1d
manavmenghwani <menghwanimanav@gmail.com>
2026-08-10 15:14:51 +0530

    Update dfa.c

--- dfa.c
+++ dfa.c
@@ -1,25 +1,35 @@
 #include<stdio.h>
 #include<string.h>
+
 int main()
 {
-char str[100];int i,len zero,one;
-printf("enter a binary string:");
-scanf("%99s",str);
-len=strlen(str);
-if(len==0)
+       char str[100];
+       int i,len;
+
+       printf("Enter binary string: ");
+       scanf("%99s", str);
+
+       len = strlen(str);
+
+       if(len == 0)
 {
-printf("string rejected\n");
-return 0;
+         printf("String rejected\n");
+         return 0; 
 }
-for(i=0;i<len;i++)
-if(str[i]!='0'&&str[i]!='1')
-{
-printf("invalid input! enter only 0 and 1,\n");
-return 0;
-}
-if(zero%2==0 && one%2==0)
-printf("string accepted\n");
-else
-printf("string rejected\n");
-return 0;
+
+      for (i=0 ;i < len ;i++) 
+      {
+          if (str[i] != '0' && str[i] !='1')
+          {
+            printf("invalid input! enter only 0 and 1.\n");
+          }
+      }  
+
+     //Accept if the string ends with 1
+     if(str[len - 1] =='1')
+       printf("string Accepted\n");
+     else
+       printf("string rejected\n");
+     return 0;
+
 }
